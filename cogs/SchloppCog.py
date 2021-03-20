94bf62937185636d29266ab5763d25d79bd26b44
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-03-20 23:13:36 +0100

    Update SchloppCog.py
    Hope this works? I edited this in the GitHub menu lmao don't hurt me

--- cogs/SchloppCog.py
+++ cogs/SchloppCog.py
@@ -7,22 +7,30 @@ class SchloppCog(utils.Cog, name="SchloppShowingHisLove"):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @utils.command(hidden=True)    
-    async def takegeorgeoutfordinner(self, ctx):
-        """Takes george out to dinner"""
+    
+    async def preform_action(self, ctx, description:str):
+        """Preform a verified gangster schlöpp action"""
         
         #Dont want everyone using this.
         if ctx.author.id != 393305855929483264 and ctx.author.id != 322542134546661388:
             return
         
         with utils.Embed(use_random_colour=True) as embed:
-            embed.description = "You take george out for a nice, romantic date in a fancy restaurant."
-
+            embed.description = description
+        
         await ctx.send(embed=embed)
 
-
-
+    @utils.command(hidden=True)    
+    async def takegeorgeoutfordinner(self, ctx):
+        """Takes george out to dinner"""
+        
+        await preform_action(ctx,"You take george out for a nice, romantic date in a fancy restaurant.")
+    
+    @utils.command(hidden=True)    
+    async def kissgeorge(self, ctx):
+        """Takes george out to dinner"""
+        
+        await preform_action(ctx, "You step out of the restaurant with george. While walking back to his place, you feel like the time is right and kiss him.")
 
 
 def setup(bot):
